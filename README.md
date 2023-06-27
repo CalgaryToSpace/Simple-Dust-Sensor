a0e4e6640e6d2734e91a103aec1b2a5913542874
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-06-27 01:12:09 -0600

    Setup dust monitor logging

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ A simple dust sensor to monitor the clean room, utilizing 7-seg displays and a w
 * This project displays the current and max dust values on the device itself, and also relays them over a network to view logs/a graph over time.
 
 ## Hardware BOM
-* [https://www.adafruit.com/product/3686](PM2.5 Air Quality Sensor and Breadboard Adapter Kit - PMS5003)
+* [PM2.5 Air Quality Sensor and Breadboard Adapter Kit - PMS5003](https://www.adafruit.com/product/3686)
+	* [Arduino Tutorial](https://learn.adafruit.com/pm25-air-quality-sensor/arduino-code)
+	* Connect TX to GPIO16 (Serial2 RX)
 * esp32doit-devkit-v1 Microcontroller Dev Kit
-* 
+	* [Pin Usage Reference](https://randomnerdtutorials.com/esp32-pinout-reference-gpios/)
+* [Adafruit 0.56" 7-segment LED HT16K33 Backpack](https://learn.adafruit.com/adafruit-led-backpack/0-dot-56-seven-segment-backpack)
+	* [Arduino Tutorial](https://learn.adafruit.com/adafruit-led-backpack/0-dot-56-seven-segment-backpack-arduino-setup)
 
 ## Credits
 * [mrWheel's YAPP Box](https://github.com/mrWheel/YAPP_Box) for the enclosure
--- Simple-Dust-Sensor-Firmware/src/main.cpp
+++ Simple-Dust-Sensor-Firmware/src/main.cpp
@@ -1,18 +1,89 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PM25AQI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// can define any I2C pins
+#define PIN_I2C_SDA 18
+#define PIN_I2C_SCL 19
+#define PIN_PM25_RX 16 // PM25 sensor TX connected to GPIO16 (MCU RX Serial2)
+
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+
+void log_pm25_data_to_serial(PM25_AQI_Data data);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(115200); // debug serial line
+	Serial.println("Starting setup()");
+
+	// wait a sec for devices to boot
+	delay(1000);
+
+	// init I2C
+	Serial.println("Init I2C");
+	Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL); // SDA, SCL
+	Serial.println("I2C init done");
+
+	// test the I2C 7segs
+	// FIXME
+
+	// init particulate sensor
+	Serial.println("Init PM 2.5 sensor");
+	Serial2.begin(9600);
+	if (! aqi.begin_UART(&Serial2)) {
+		Serial.println("Could not find PM 2.5 sensor!");
+		while (1) delay(100);
+	}
+	Serial.println("PM 2.5 sensor found and connected!");
+
+
+  
+	Serial.println("Done setup()");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	Serial.println("Starting loop()");
+
+	// read data
+	PM25_AQI_Data data;
+	if (! aqi.read(&data)) {
+		Serial.println("Could not read from AQI");
+		delay(500);  // try again in a bit!
+		return;
+	}
+	Serial.println("Read from AQI successful.");
+
+	// log data
+	log_pm25_data_to_serial(data);
+
+	// TODO update on screen
+
+	// wait a bit
+	delay(1000);
+
+	Serial.println("End loop()");
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void log_pm25_data_to_serial(PM25_AQI_Data data) {
+	Serial.println();
+	Serial.println(F("---------------------------------------"));
+	Serial.println(F("Concentration Units (standard)"));
+	//Serial.println(F("---------------------------------------"));
+	Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
+	Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
+	Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
+	Serial.println(F("Concentration Units (environmental)"));
+	//Serial.println(F("---------------------------------------"));
+	Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
+	Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
+	Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
+	Serial.println(F("---------------------------------------"));
+	Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
+	Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
+	Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
+	Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
+	Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
+	Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
+	Serial.println(F("---------------------------------------"));
 }
